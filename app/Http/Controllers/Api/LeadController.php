736198c7c26e9feac56c927bf5d35c36a17baf4e
iaio998 <depalatisiacopo@gmail.com>
2024-01-24 10:44:24 +0100

    Added lead rcm

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'message' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lead $lead)
+    {
+        //
+    }
+}
